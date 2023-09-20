0f2bb84afd62ee9fa2c319014618ec4efcdff376
lesclaz <lesclaz95@gmail.com>
2023-09-20 03:04:10 -0400

    try fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,8 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
